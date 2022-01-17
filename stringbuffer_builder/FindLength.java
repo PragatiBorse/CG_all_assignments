fce5b71a9c75b91eeaee3409c63ec1d140c39a22
Pragati Borse <58590688+PragatiBorse@users.noreply.github.com>
2022-01-17 22:14:33 +0530

    Add files via upload

---
+++ stringbuffer_builder/FindLength.java
@@ -0,0 +1,15 @@
+/*write an application to determine the length of the string str="Hello World"*/
+package com.stringbuffer_builder;
+
+public class FindLength {
+
+	public static void main(String[] args) {
+
+		String str="Hello World";
+		int n=str.length();
+		System.out.println("Length is: "+str.length());
+		
+        
+	}
+
+}
---
+++ stringbuffer_builder/JoinString.java
@@ -0,0 +1,13 @@
+package com.stringbuffer_builder;
+
+public class JoinString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1="Hello";
+		String s2=" How Are You?";
+		
+		System.out.println(s1.concat(s2));
+	}
+
+}
